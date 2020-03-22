80893abd3fee0633ee08f55597809bed27334756
Jam Risser <jam@codejam.ninja>
2020-03-22 20:14:02 +0530

    Updates

---
+++ web/precache-manifest.cbfa45aaac27d71a240775f7d225d7de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19d1e8f0b49437481d2dc1b71c63299",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9efe29b62f2204d30ce",
+    "url": "/static/css/main.3baaee0e.chunk.css"
+  },
+  {
+    "revision": "d57ea7ef84eeb3eb9475",
+    "url": "/static/js/2.b0bbe7ab.chunk.js"
+  },
+  {
+    "revision": "0fef09f97c30ad25cb44221d280778ce",
+    "url": "/static/js/2.b0bbe7ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9efe29b62f2204d30ce",
+    "url": "/static/js/main.28781eef.chunk.js"
+  },
+  {
+    "revision": "852a701ed1e761d416c4",
+    "url": "/static/js/runtime-main.f3e146ed.js"
+  }
+]);
