809ca5193ca3119431d3b6b36c090e8ed013a0f4
Jam Risser <jam@codejam.ninja>
2020-03-24 21:48:22 +0530

    Updates

--- precache-manifest.f13b1b99dde63e12b555ddc039b7aaaf.js
+++ precache-manifest.f13b1b99dde63e12b555ddc039b7aaaf.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2e3d0a586d2697b7a9b60bba0a38dc98",
+    "revision": "98d71298df5dab4ade3c21815c816db3",
     "url": "./index.html"
   },
   {
-    "revision": "5a3de7ceffa7afdaae3d",
+    "revision": "d93b0dfbc3b6912e5d19",
     "url": "./static/css/main.3baaee0e.chunk.css"
   },
   {
-    "revision": "fa3b8b4aa7cd2d463581",
-    "url": "./static/js/2.67bdee16.chunk.js"
+    "revision": "e174c845ce2ce881a7b9",
+    "url": "./static/js/2.da424443.chunk.js"
   },
   {
     "revision": "04ffedcc0d7c55000e35bb5bf9fd88a0",
-    "url": "./static/js/2.67bdee16.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.da424443.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5a3de7ceffa7afdaae3d",
-    "url": "./static/js/main.18a0e334.chunk.js"
+    "revision": "d93b0dfbc3b6912e5d19",
+    "url": "./static/js/main.0e0d9121.chunk.js"
   },
   {
     "revision": "5af45e65bc3439f207cc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a0ad84aa3de5e06cfe4f72bca651533f.js"
+  "./precache-manifest.f13b1b99dde63e12b555ddc039b7aaaf.js"
 );
 
 self.addEventListener('message', (event) => {
