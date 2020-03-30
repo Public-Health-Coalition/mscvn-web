e054ea5a2d2987490d5bbed3fbe7c78d6a42ffca
Jam Risser <jam@codejam.ninja>
2020-03-30 22:55:25 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5dd8ecc8abbbb95d1c14.js"
+    "url": "webpack-runtime-14d0f20dadbd8f83e4ba.js"
   },
   {
     "url": "styles.e691cdcde8291515db55.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f628266976bd8969a66d75fc7a82d81b"
+    "revision": "361d05826b7ac11937ac0649abf3ea43"
   },
   {
     "url": "google-fonts/s/firasans/v10/va9B4kDNxMZdWfMOD5VnLK3eRhf6.woff2",
