44de13edc41b41f6122ff18ffe2b6b3353452073
Josh Austin <joshaustin@laptop.local>
2020-03-29 22:50:50 -0500

    Added Google Analytics code

--- gatsby-config.js
+++ gatsby-config.js
@@ -110,7 +110,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: "UA-161712632-1",
       }
     },
     {
