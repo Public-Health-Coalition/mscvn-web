d1d213e6b7b6b6fee9a52d3048e44fce1f025edd
Jam Risser <jam@codejam.ninja>
2020-04-13 16:01:55 +0000

    Removed service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -132,7 +132,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-offline`,
+      resolve: `gatsby-plugin-remove-serviceworker`,
     },
     {
       resolve: `gatsby-plugin-react-helmet`,
