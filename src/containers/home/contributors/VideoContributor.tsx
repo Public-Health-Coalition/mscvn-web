a3d7679cdec897cc0161a5bd32d7c8e4a9e3720c
vle115 <vle2116@gmail.com>
2020-06-06 15:06:44 -0400

    Create VideoContributor.tsx

---
+++ src/containers/home/contributors/VideoContributor.tsx
@@ -0,0 +1,58 @@
+import React, { FC } from 'react';
+import { Box, Flex, Heading, Text, Link } from '@primer/components';
+
+export interface VideoContributorProps {
+  bio?: string;
+  homepage?: string;
+  name: string;
+  photo?: string;
+  title?: string;
+}
+
+const VideoContributor: FC<VideoContributorProps> = (props: VideoContributorProps) => {
+  function renderPhoto() {
+    if (!props.photo) return null;
+    return (
+      <Link href={props.homepage} target="_blank" mb={2}>
+        <Flex justifyContent="center" mb={20}>
+          <img
+            src={props.photo}
+            style={{ borderRadius: '100%', maxWidth: 140, maxHeight: 140 }}
+          />
+        </Flex>
+      </Link>
+    );
+  }
+
+  return (
+    <Box mb={60} ml={3} mr={3}>
+      {renderPhoto()}
+      <Heading textAlign="center" fontSize={5} mb={2}>
+        {props.name}
+      </Heading>
+      <Heading textAlign="center" fontSize={2} mb={2}>
+        {props.title}
+      </Heading>
+      <Text textAlign="center">
+        <Link href={props.homepage} target="_blank" mb={2}>
+          <Heading fontSize={1} textAlign="center">
+            {props.homepage}
+          </Heading>
+        </Link>
+        <Box mb={2} />
+        <Box textAlign="center">
+          <Text fontSize={2}>{props.bio}</Text>
+        </Box>
+      </Text>
+    </Box>
+  );
+};
+
+VideoContributor.defaultProps = {
+  bio: '',
+  homepage: '',
+  photo: '',
+  title: ''
+};
+
+export default VideoContributor;
